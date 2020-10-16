481a10582681bfe944cf7b0c3a2246873da586bf
Thiago Gaitkoski <thiagoweb.dvelo@gmail.com>
2020-10-16 20:21:42 -0300

    Ajuste do server .JSON

--- src/server.js
+++ src/server.js
@@ -1,1 +1,17 @@
-console.log('');
+//IMPORT LIB
+const express = require('express');
+const path = require('path');
+
+//INICIATE THE EXPRESS
+const server = express()
+server
+//USING STATIC ARCHIVES
+.use(express.static('public'))
+
+//CREATE ROUT
+.get('/',(request, response) => {
+    return response.sendFile(path.join(__dirname, 'views', 'index.html'))
+})
+
+//TURN ON SERVER
+server.listen(5500)
